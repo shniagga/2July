159bb281fb8e7498db19abf098e1ca721304a6b4
Yura Luchkiv <netex2009@gmail.com>
2012-05-12 17:35:38 +0300

    create LauchActivity with background photo and background music add images and music

--- 2July/src/org/cats/LaunchActivity.java
+++ 2July/src/org/cats/LaunchActivity.java
@@ -1,13 +1,38 @@
 package org.cats;
 
 import android.app.Activity;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 public class LaunchActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+	private final static int LAUNCH_TIME = 10000; // miliseconds
+	private MediaPlayer launchSong;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.launch);
+		launchSong = MediaPlayer.create(LaunchActivity.this, R.raw.background);
+		launchSong.start();
+
+		Thread timer = new Thread() {
+			public void run() {
+				try {
+					sleep(LAUNCH_TIME);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				} finally {
+					// TODO Intent
+				}
+			}
+		};
+		timer.start();
+	}
+	
+	@Override
+	protected void onPause() {
+		super.onPause();
+		launchSong.release();
+		finish();
+	}
 }
