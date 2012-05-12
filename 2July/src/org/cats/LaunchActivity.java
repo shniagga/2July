68fb7b44f5f65eabf3bbd2ceebda89015f83bccb
Yura Luchkiv <netex2009@gmail.com>
2012-05-12 17:13:34 +0300

    create 2July project

---
+++ 2July/src/org/cats/LaunchActivity.java
@@ -0,0 +1,13 @@
+package org.cats;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LaunchActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
